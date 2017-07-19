13c1e45761b843ca534a47ce70ee45d437fd4038
ashmeet <ashmeet4293@gmail.com>
2017-07-19 18:05:25 +0545

    Updated code

--- src/main/java/com/pinet/app/service/UserServiceImpl.java
+++ src/main/java/com/pinet/app/service/UserServiceImpl.java
@@ -24,14 +24,14 @@ public class UserServiceImpl implements UserInterface{
     @Override
     public String insertUser() {
         UserDataEntity userDataEntity=new UserDataEntity();
-        userDataEntity.setName("Ashmeet");
-        userDataEntity.setUsername("ashmeet4293");
+        userDataEntity.setName("Arjun ");
+        userDataEntity.setUsername("arjun4293");
         userDataEntity.setDob(new Date());
         userDataEntity.setGender('M');
         userDataEntity.setNationality("Nepali");
-        userDataEntity.setCitizenNo("101-10101-101001");
+        userDataEntity.setCitizenNo("101-122101-101001");
         userDataEntity.setPassportNo("202-202-0222-22");
-        userDataEntity.setFatherName("Pawan Tiwary");
+        userDataEntity.setFatherName("Arjun Pade");
         userDataEntity.setEmail("ashmeet4293@gmail.com");
         userDataEntity.setAddress("Pokhara");
         userDataEntity.setMobileNo("9846744037");
