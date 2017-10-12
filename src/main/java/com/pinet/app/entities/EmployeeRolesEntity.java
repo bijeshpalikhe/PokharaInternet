70d12c5d0cdba35701faacc86c261cc42d56ff05
bijeshpalikhe <bijesh.palikhe@gmail.com>
2017-10-12 12:01:53 +0545

    adding new tables

---
+++ src/main/java/com/pinet/app/entities/EmployeeRolesEntity.java
@@ -0,0 +1,173 @@
+package com.pinet.app.entities;
+
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+        /*
+        *Created by bijesh on 7/17/2017.
+        *Table to store Employee info
+        * */
+
+
+@Entity
+@Table(name = "employee_roles")
+public class EmployeeRolesEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "employee_id")
+    private Integer employeeId;
+
+    @Column(name = "phone_no")
+    private String phoneNo;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "info")
+    private String info;
+
+    @Column(name = "dob")
+    private Date dob;
+
+    @Column(name = "id_type")
+    private String idType;
+
+    @Column(name = "id_no")
+    private String idNo;
+
+    @Column(name = "file_no")
+    private String fileNo;
+
+    @Column(name = "employee_name")
+    private String employeesName;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private Date createdDate;
+
+
+    @Column(name = "last_modified_by")
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedBy
+    @Column(name = "last_modified_date")
+    private Date lastModifiedDate;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getIdType() {
+        return idType;
+    }
+
+    public void setIdType(String idType) {
+        this.idType = idType;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public String getFileNo() {
+        return fileNo;
+    }
+
+    public void setFileNo(String fileNo) {
+        this.fileNo = fileNo;
+    }
+
+    public String getEmployeesName() {
+        return employeesName;
+    }
+
+    public void setEmployeesName(String employeesName) {
+        this.employeesName = employeesName;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+}
