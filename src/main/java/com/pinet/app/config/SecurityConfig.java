e45ee68ffabfba68edbe94d65983d2b585d2897a
ashmeet4293 <ashmeet4293@gmail.com>
2017-12-31 10:36:17 +0545

    HEroku

--- src/main/java/com/pinet/app/config/SecurityConfig.java
+++ src/main/java/com/pinet/app/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.pinet.app.security.web.LoggingAccessDeniedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
--- src/main/java/com/pinet/app/web/controller/ExceptionHandlingController.java
+++ src/main/java/com/pinet/app/web/controller/ExceptionHandlingController.java
@@ -1,6 +1,5 @@
 package com.pinet.app.web.controller;
 
-import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
